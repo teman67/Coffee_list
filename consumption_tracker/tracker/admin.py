bff9d1bcb09ea7f07b6bc2945704d2356b15e39c
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2024-06-30 20:58:05 +0000

    removing all the test files

--- consumption_tracker/tracker/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Consumption
-
-admin.site.register(Consumption)
--- consumption_tracker/tracker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import profile, delete_account
-
-
-urlpatterns = [
-    path('profile/', profile, name='profile'),
-    path('delete-account/', delete_account, name='delete_account'),
-]
